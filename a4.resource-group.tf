2afc59462039494af5c1ae497c5c7e244f6f2598
gopal <gopal1409@gmail.com>
2022-12-28 10:31:26 +0530

    terraform resource group block

--- a4.resource-group.tf
+++ a4.resource-group.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "${resource_name_prefix}-${var.resource_group}"
+  name     = "${local.resource_name_prefix}-${var.resource_group}"
   location = var.resource_group_location
   tags = local.common_tags
 }
