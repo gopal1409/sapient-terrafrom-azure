b500085218d6e01c1326c498ee04310e0980c4b0
gopal <gopal1409@gmail.com>
2022-12-30 14:39:49 +0530

    terraform subnet-nsg-nsgassocation added

--- a10.vm.tf
+++ a10.vm.tf
@@ -23,7 +23,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  size                = "Standard_DS1-v2"
+  size                = "Standard_DS1_v2"
   admin_username      = "azureuser"
   network_interface_ids = [
     azurerm_network_interface.web_linuxvm_nic.id,
