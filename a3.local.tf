da9298bfb7d28cd46055d94becca44c75c065317
gopal <gopal1409@gmail.com>
2022-12-30 11:29:55 +0530

    terraform local

--- a3.local.tf
+++ a3.local.tf
@@ -2,7 +2,7 @@ locals {
   #the purpose of local values is to do concatination of multiple varaiblename 
   owners = var.business_division
   environment = var.environment
-  #3now we want to concatinate above mentioned varaible together
+  #3now we want to concatinate above mentioned varaible togetherasda
   resource_name_prefix = "${var.business_division}-${var.environment}"
   common_tags = {
     owners = local.owners
