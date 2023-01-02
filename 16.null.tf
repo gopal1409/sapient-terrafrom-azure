574690add377c8a832778e290485516c29dacb1e
gopal <gopal1409@gmail.com>
2023-01-02 12:15:25 +0530

    null provisioner

--- 16.null.tf
+++ 16.null.tf
@@ -6,12 +6,12 @@ resource "null_resource" "null_copy_ssh_key_to_bastion" {
     type = "ssh"
     host = azurerm_linux_virtual_machine.bastion_host_linuxvm.public_ip_address
     user = azurerm_linux_virtual_machine.bastion_host_linuxvm.admin_username
-    private_key = file("~/.ssh/id_rsa.pub")
+    private_key = file("~/.ssh/id_rsa")
   }
   #file provisioiner which will upload my key
   provisioner "file" {
-    source = "~/.ssh/id_rsa.pub"
-    destination = "/tmp/id_rsa.pub"
+    source = "~/.ssh/id_rsa"
+    destination = "/tmp/id_rsa"
   }
   provisioner "remote-exec" {
     inline = [
