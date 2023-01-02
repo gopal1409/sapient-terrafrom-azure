61332e5e9a81737c506ed18064f35f5aa1ad4d6c
gopal <gopal1409@gmail.com>
2023-01-02 10:19:13 +0530

    terraform module path

--- a12.output-weblinux.tf
+++ a12.output-weblinux.tf
@@ -1,4 +1,4 @@
-output "web_linuxvm_private_ip_address.tf" {
+output "web_linuxvm_private_ip_address" {
   description = "web linux vm private ip"
   value = azurerm_linux_virtual_machine.example.private_ip_address
 }
