372272f1ac7cdd986e6324a2a4d59878ed03af16
gopal <gopal1409@gmail.com>
2023-01-02 11:02:33 +0530

    terraform bastion resource

--- a14.bastion-host-input.tf
+++ a14.bastion-host-input.tf
@@ -4,3 +4,12 @@ variable "bastion_service_subnet_name" {
 variable "bastion_service_address_prefixes" {
   value = ["10.0.101.0/27"]
 }
+variable "bastion_subnet_name" {
+  type = string 
+  value = "bastionsubnet"
+}
+
+variable "bastion_subnet_address" {
+  type = list[string]
+  value = ["10.0.100.0/24"]
+}
