2ef0f79d94c541cd2b916ec74964d25c8d0ce388
gopal <gopal1409@gmail.com>
2023-01-02 15:11:04 +0530

    terraform tfsate remote

--- a2.provider.tf
+++ a2.provider.tf
@@ -11,13 +11,14 @@ terraform {
     null = {
       source = "hashicorp/null"
     }
-    backend "azurerm" {
+    
+  }
+  backend "azurerm" {
       resource_group_name = "terraformstorage"
       storage_account_name = "terraformtfstate009"
       container_name = "terraformstate201"
       key = "project-1-eastus-teraform.tfstate"
     }
-  }
 }
 
 # Configure the Microsoft Azure Provider
