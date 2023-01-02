8f5c6a47e7d7764c583cb85c76c559c7a27329a7
gopal <gopal1409@gmail.com>
2023-01-02 12:03:24 +0530

    null provisioner

--- 16.null.tf
+++ 16.null.tf
@@ -5,7 +5,7 @@ resource "null_resource" "null_copy_ssh_key_to_bastion" {
   connection {
     type = "ssh"
     host = azurerm_linux_virtual_machine.bastion_host_linuxvm.public_ip_address
-    user = azurerm_linux_virtual_machine.bastion_host_linuxvm.admin_user 
+    user = azurerm_linux_virtual_machine.bastion_host_linuxvm.admin_username
     private_key = file("~/.ssh/id_rsa.pub")
   }
   #file provisioiner which will upload my key
